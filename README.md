0753b6b4fcf6de6f3890e99a507f61902482a60d
wubinjie <wubinjie@micro-helix.com>
2016-04-22 14:12:27 +0800

    add eslint with gulp tool

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # jshint, eslint 的 gulp 工具绑定
 
+[ESLint](http://eslint.org/)
 
 ## jshint 与 eslint
 
 
 ## 与gulp 工具的结合
+
+- jshint: `gulp-eslint`
---
+++ eslint/rule-test1.js
@@ -0,0 +1,15 @@
+// Error no-unused-vars
+var cc = {
+    a:1,
+    b:2
+};
+
+// quotes
+// Warning  about quotes
+// var c = "12";
+
+
+var c = '12';
+
+console.log(c);
+console.log(cc);
